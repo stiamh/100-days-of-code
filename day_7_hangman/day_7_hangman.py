d21c040148503bcf8bf30247dbd81d6e3f817959
Stephen-Furey <stephenfurey44@gmail.com>
2022-01-13 16:48:02 +0000

    Small game updates
    changed blackjack file name to and made some changes to hangman to make it replayable.

--- day_7_hangman/day_7_hangman.py
+++ day_7_hangman/day_7_hangman.py
@@ -2,10 +2,6 @@ import random
 import art
 import word_list
 from os import system, name
-chosen_word = random.choice(word_list.word_list)
-word_length = len(chosen_word)
-end_of_game = False
-lives = 6
 
 def clear():
     if name == "nt":
@@ -13,38 +9,47 @@ def clear():
     else:
         _ = system("clear")#For mac users
 
-print(art.logo)
+def game():
+    chosen_word = random.choice(word_list.word_list)
+    word_length = len(chosen_word)
+    end_of_game = False
+    lives = 6
 
-display = []
-already_guessed = []
-for _ in range(word_length):
-    display += "_"
+    print(art.logo)
 
-while not end_of_game:
-    guess = input("Guess a letter: \n").lower()
-    clear()
-    if guess in already_guessed:
-        print(f"You have already guessed {guess}, please try another letter. \n")
-        print(art.stages[lives])
-    else:
-        for position in range(word_length):
-            letter = chosen_word[position]
-            if letter == guess:
-                display[position] = letter
+    display = []
+    already_guessed = []
+    for _ in range(word_length):
+        display += "_"
 
-        if guess not in chosen_word:
-            lives -= 1
-            print(f"\n'{guess}' is not in the word, you have {lives} lives remaining.\n")
-            if lives == 0:
-                end_of_game = True
-                print("You lose.")
+    while not end_of_game:
+        guess = input("Guess a letter:").lower()
+        clear()
+        if guess in already_guessed:
+            print(f"You have already guessed {guess}, please try another letter. \n")
+        else:
+            for position in range(word_length):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+
+            if guess not in chosen_word:
+                lives -= 1
+                print(f"\n'{guess}' is not in the word, you have {lives} lives remaining.\n")
+                if lives == 0:
+                    print(f"You lose. The word was {chosen_word}")
+                    end_of_game = True
+            already_guessed.append(guess)
+     
 
         print(f"\n{' '.join(display)}")
 
         if "_" not in display:
             end_of_game = True
             print("You win.")
-
-        already_guessed.append(guess)
         print(art.stages[lives])
         print(f"Guessed letters: {already_guessed}")
+game()
+while input("\nDo you want to play another game? Type 'y' or 'n': \n") == "y":
+    clear()
+    game()
